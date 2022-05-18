d69d2c63c1fd5d4abd409c631f104c90f2af4f2f
sangaaitech <sangaaitech@gmail.com>
2022-05-18 22:08:37 +0530

    commit for card 1

--- pages/login-t.jsx
+++ pages/login-t.jsx
@@ -14,7 +14,7 @@ const LoginT = () => {
               >
                 <path
                   fill="#ffffff"
-                  fill-opacity="1"
+                  fillOpacity="1"
                   d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                 ></path>
               </svg>
@@ -33,7 +33,7 @@ const LoginT = () => {
                     placeholder="john@doe.com"
                   />
                   <label
-                    for="email"
+                    htmlFor="email"
                     className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Email address
@@ -48,7 +48,7 @@ const LoginT = () => {
                     placeholder="Password"
                   />
                   <label
-                    for="password"
+                    htmlFor="password"
                     className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Password
